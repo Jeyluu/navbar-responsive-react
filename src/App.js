50342d3c8e2350fc790222210df2b377e0c19e01
Jérémy Luu <73641462+Jeyluu@users.noreply.github.com>
2021-09-30 15:44:58 +0200

    navbar fin

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import {useState, useEffect, useRef} from 'react'
+import './App.css';
+import Navbar from './Component/Navbar/Navbar';
+
+
+
+function App() {
+
+
+  return (
+    /* Utilisation de react Fragment*/
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
